99a0248d28b3fff435d0870465e6c4983083cf39
dplong <longdo805@gmail.com>
2025-04-03 15:33:10 +0700

    chore: add TODO

--- ios/Classes/AudioPlayer.swift
+++ ios/Classes/AudioPlayer.swift
@@ -147,6 +147,7 @@ class AudioPlayer: NSObject, AVAudioPlayerDelegate {
         }else{
             self.finishMode = FinishMode.stop
         }
+        // TODO: return value for flutter channel
         result(true)
     }
 
